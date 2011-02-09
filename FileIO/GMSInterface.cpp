2afe6fa899cefa16c602281efc90aca35e246088
Karsten Rink <karsten.rink@ufz.de>
2011-02-09 16:09:23 +0100

    fixed bug in GMS interface: read method ignored last borehole in file

--- FileIO/GMSInterface.cpp
+++ FileIO/GMSInterface.cpp
@@ -63,6 +63,11 @@ int GMSInterface::readBoreholesFromGMS(std::vector<GEOLIB::Point*> *boreholes, c
 		else
 			std::cout << "GMSInterface::readBoreholeFromGMS() - Error reading format..." << std::endl;
 	}
+	if (newBorehole != NULL)
+	{
+		newBorehole->setDepth((*newBorehole)[2]-depth);
+		boreholes->push_back(newBorehole);
+	}
 
 	in.close();
 	return 1;
