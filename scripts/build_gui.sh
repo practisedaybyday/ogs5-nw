25b12163e766308776fd5d8f76940da1a9bb8f21
Lars Bilke <lars.bilke@ufz.de>
2011-02-10 17:09:35 +0100

    Modified linux gui build script

--- scripts/build_gui.sh
+++ scripts/build_gui.sh
@@ -7,7 +7,10 @@ cd ..
 rm -rf build_gui
 
 mkdir -p build_gui && cd build_gui
-../scripts/cmake.ogs.sh -DOGS_USE_QT=ON -DOGS_PACKAGING=ON ..
+#../scripts/cmake.ogs.sh -DOGS_USE_QT=ON -DOGS_PACKAGING=ON ..
+cmake -DOGS_USE_QT=ON -DOGS_PACKAGING=ON ..
 make -j
-../scripts/cmake.ogs.sh ..
-make package -j
+#../scripts/cmake.ogs.sh ..
+cmake ..
+make -j
+make package
