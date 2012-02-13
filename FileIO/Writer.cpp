a9323b6ec1ebf20ceda611909a6418f586268598
Lars Bilke <lars.bilke@ufz.de>
2012-02-13 16:23:55 +0100

    Added Writer base class.

---
+++ FileIO/Writer.cpp
@@ -0,0 +1,58 @@
+/**
+ * \file Writer.cpp
+ * 13/02/2012 LB Initial implementation
+ * 
+ * Implementation of the Writer class
+ */
+
+// ** INCLUDES **
+#include "Writer.h"
+
+#include <fstream>
+
+namespace FileIO
+{
+
+Writer::Writer()
+{
+	
+}
+
+std::string Writer::writeToString()
+{
+	// Empty stream and clear error states.
+	_stream.str("");
+	_stream.clear();
+	
+	this->write(_stream);
+	return _stream.str();
+}
+
+void Writer::writeToFile(std::string filename)
+{
+	std::ofstream fileStream;
+	fileStream.open (filename.c_str());
+	
+	// check file stream
+	if (!fileStream)
+	{
+		std::cerr << "Could not open file " << filename << " !" << std::endl;
+		return;
+	}
+
+	fileStream << this->writeToString();
+
+	fileStream.close();
+}
+
+void Writer::setPrecision(unsigned int precision)
+{
+	_stream.precision(precision);
+}
+
+void Writer::setFormat(std::ios_base::fmtflags flags)
+{
+	_stream.setf(flags);
+}
+
+} // namespace FileIO
