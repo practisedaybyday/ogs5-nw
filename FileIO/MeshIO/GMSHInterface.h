94efdac7ec75e9fe3349b76bf70ecc2bbd778a61
Thomas Fischer <thomas.fischer@ufz.de>
2012-04-17 16:11:46 +0200

    removed old code from GMSHInterface

--- FileIO/MeshIO/GMSHInterface.h
+++ FileIO/MeshIO/GMSHInterface.h
@@ -14,7 +14,6 @@
 // FileIO
 #include "Writer.h"
 #include "GMSHPoint.h"
-#include "GMSHLineLoop.h"
 #include "GMSHPolygonTree.h"
 #include "GMSHMeshDensityStrategy.h"
 
@@ -91,40 +90,6 @@ private:
 	void writeGMSHInputFile(std::ostream & out);
 
 	void writePoints(std::ostream& out) const;
-//	void writeLines(std::ostream& out) const;
-
-	/**
-	 * create vector of points
-	 * @param bounding_polygon
-	 * @param pnt_vec
-	 * @param station
-	 * @return
-	 */
-//	size_t createGMSHPointsInsideBoundingPolygon(GEOLIB::Polygon const*const bounding_polygon, const std::vector<GEOLIB::Point*> &pnt_vec, bool station=false);
-
-
-//	std::list<size_t> findHolesInsidePolygon(const std::vector<GEOLIB::Polyline*>* plys, size_t i) const;
-	/**
-	 * Method tests if the i-th polygon is included in any other polygon.
-	 * @param plys the vector of polylines (closed polylines are considered as polygon)
-	 * @param i the polygon for the test
-	 * @param j if the method returns true, the j-th polygon contains the i-th polygon
-	 * @return true, if polygon is in an other polygon included, else false
-	 */
-//	bool isPolygonInOtherPolygon(const std::vector<GEOLIB::Polyline*>* plys, size_t i, size_t &j) const;
-
-//	GEOLIB::Polygon* getBoundingPolygon (std::vector<GEOLIB::Polyline*> const & all_polylines,
-//	                                     size_t &bp_idx) const;
-//	void getStationPoints(const GEOLIB::GEOObjects &geo_objects, std::vector<GEOLIB::Point*>& station_points) const;
-//	void getSteinerPoints(GEOLIB::QuadTree<GEOLIB::Point>* quad_tree, std::vector<GEOLIB::Point*>& steiner_pnts) const;
-//	void createBoundingPolygon(GEOLIB::Polygon const* const bounding_polygon, size_t pnt_offset);
-
-	/// Adds a point-array (such as stations) as constraints to the geometry given by proj_name.
-//	void addPointsAsConstraints(const std::vector<GEOLIB::Point*> &points,
-//					const std::vector<GEOLIB::Polyline*> &polylines, std::ostream &out);
-
-//	void createPolylinesAsConstraints(GEOLIB::Polygon * bounding_polygon, size_t bp_idx,
-//					std::vector<GEOLIB::Polyline*> const& all_polylines, std::ostream & out);
 
 	size_t _n_lines;
 	size_t _n_plane_sfc;
@@ -132,34 +97,13 @@ private:
 	GEOLIB::GEOObjects & _geo_objs;
 	std::vector<std::string>& _selected_geometries;
 	std::string _gmsh_geo_name;
-	std::string _gmsh_station_name;
 	std::list<GMSHPolygonTree*> _polygon_tree_list;
 
 	bool _include_stations_as_constraints;
 
-//	std::vector<size_t> _gmsh_pnt_id_to_geo_pnt_id_map;
 	std::vector<FileIO::GMSHPoint*> _gmsh_pnts;
-	std::vector<FileIO::GMSHLineLoop*> _gmsh_line_loops;
 
 	GMSHMeshDensityStrategy *_mesh_density_strategy;
-
-//	/**
-//	 * this private class is for storing meta data, i.e. the mapping between
-//	 * polyline id, the number of the first and the last line segments
-//	 * in gmsh file that are describing the polyline in the gmsh file
-//	 */
-//	struct PolylineGMSHMapping {
-//		PolylineGMSHMapping (size_t ply_id, size_t start_id, size_t end_id) :
-//			_ply_id (ply_id), _gmsh_line_start_id(start_id), _gmsh_line_end_id(end_id)
-//		{}
-//		size_t _ply_id;
-//		size_t _gmsh_line_start_id;
-//		size_t _gmsh_line_end_id;
-//	};
-//	/**
-//	 * vector contains the mapping between polyline ids and first and last segment in gmsh file
-//	 */
-//	std::vector<PolylineGMSHMapping> _ply_id_gmsh_line_mapping;
 };
 }
 
