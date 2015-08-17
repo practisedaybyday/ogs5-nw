64841ec81fd7fc5c4658285ac0916ff70fe9822b
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2015-08-17 14:16:35 +0200

    add CMP0054 if cmake >= 3.1

--- scripts/CMakeConfiguration/CMakeSetup.cmake
+++ scripts/CMakeConfiguration/CMakeSetup.cmake
@@ -1,4 +1,15 @@
 
+IF (NOT (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 3.1))
+	CMAKE_POLICY(SET CMP0054 OLD)
+ENDIF()
+# Suppress warning on setting policies
+CMAKE_POLICY(SET CMP0011 OLD)
+
+# Suppress warning on add_subdirectory(dir) where dir contains no CMakeLists.txt
+IF (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} GREATER 2.7)
+	CMAKE_POLICY(SET CMP0014 OLD)
+ENDIF ()
+
 # Set additional CMake modules path
 SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/scripts/CMakeConfiguration")
 LIST(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/scripts/cmake")
@@ -11,14 +22,6 @@ INCLUDE(CppcheckTargets)
 # Adds useful macros and variables
 INCLUDE(Macros)
 
-
-# Suppress warning on setting policies
-CMAKE_POLICY(SET CMP0011 OLD)
-
-# Suppress warning on add_subdirectory(dir) where dir contains no CMakeLists.txt
-IF (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} GREATER 2.7)
-	CMAKE_POLICY(SET CMP0014 OLD)
-ENDIF ()
 
 # Provide a way for Visual Studio Express users to turn OFF the new FOLDER
 # organization feature. Default to ON for non-Express users. Express users must
